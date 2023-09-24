ba4f84812b5662cd1c4e8cc418775249167627d6
Andrew112236 <102410886+Andrew112236@users.noreply.github.com>
2023-09-24 20:18:38 +0300

    react-finals

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ImageGallery.module.css';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 
 export default function ImageGallery({ images, onImageClick }) {
+  const [setSelectedImage] = useState(null);
+
+  const handleImageClick = imageId => {
+    setSelectedImage(imageId);
+    onImageClick(imageId);
+  };
+
   return (
-    <ul className={styles.gallery}>
-      {images.map(image => (
-        <ImageGalleryItem
-          key={image.id}
-          imageUrl={image.webformatURL}
-          onClick={() => onImageClick(image.largeimageUrl)}
-        />
-      ))}
-    </ul>
+    <div>
+      <ul className={styles.gallery}>
+        {images.map(image => (
+          <ImageGalleryItem
+            key={image.id}
+            imageUrl={image.webformatURL}
+            onClick={() => handleImageClick(image.largeimageUrl)}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
